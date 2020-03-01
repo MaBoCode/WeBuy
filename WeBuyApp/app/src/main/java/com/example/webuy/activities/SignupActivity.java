3b1099522300d00a51ba33e98fc4c535dde1a187
Matthias Brown <matthias.brown@etu.univ-tours.fr>
2020-03-02 00:36:53 +0100

    Added Signup, Login activities

--- WeBuyApp/app/src/main/java/com/example/webuy/activities/SignupActivity.java
+++ WeBuyApp/app/src/main/java/com/example/webuy/activities/SignupActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.webuy.R;
 
-public class MainActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup);
     }
 }

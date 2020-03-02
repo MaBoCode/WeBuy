1f0b5526a2a287d78713b99ff2df8e611a75b8e9
Matthias <matthias.brown@etu.univ-tours.fr>
2020-03-02 17:52:11 +0100

    Added Tab layout

--- WeBuyApp/app/src/main/java/com/example/webuy/activities/AuthenticationActivity.java
+++ WeBuyApp/app/src/main/java/com/example/webuy/activities/AuthenticationActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.webuy.R;
 
-public class SignupActivity extends AppCompatActivity {
+public class AuthenticationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_main);
     }
 }
